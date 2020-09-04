57ec332da5b38f170d7b320d432e84fbf79f0e59
whbn8888 <wuhang@88.com>
2020-09-04 16:57:54 +0800

    test

--- src/main/java/com/mr/HiWord.java
+++ src/main/java/com/mr/HiWord.java
@@ -2,6 +2,6 @@ package com.mr;
 
 public class HiWord {
     public static void main(String[] args) {
-        System.out.println("Hi Word wuhang he jinzai he wangbo");
+        System.out.println("Hi Word wuhang he jinzai he wangbo hedaniuzi");
     }
 }
